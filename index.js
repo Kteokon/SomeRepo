111f33a8369219939009d744301e3dfd43f459d4
”Maksimova <ktoenok0801@gmail.com>
2022-09-20 14:22:21 +0800

    Created TypeScript program

--- index.js
+++ index.js
@@ -11,4 +11,4 @@ console.log("Time left before New Year: " + Math.floor((dateNewYear - dateNow) /
 	+ Math.floor((dateNewYear - dateNow) % (days) % (hours) / (mins)) + " minutes "
 	+ Math.floor((dateNewYear - dateNow) % (days) % (hours) % (mins) / (secs)) + " seconds");
 	
-console.lod("Updated.");
+console.log("Updated.");
---
+++ index.ts
@@ -0,0 +1,14 @@
+const dateNow = new Date();
+const dateNewYear = new Date(dateNow.getFullYear() + 1, 0, 1, 0, 0, 0, 0);
+	
+const secs = 1000,
+	mins = secs * 60,
+	hours = mins * 60,
+	days = hours * 24;
+
+console.log("Time left before New Year: " + Math.floor((dateNewYear.getTime() - dateNow.getTime()) / (days)) + " days "
+	+ Math.floor((dateNewYear.getTime() - dateNow.getTime()) % (days) / (hours)) + " hours "
+	+ Math.floor((dateNewYear.getTime() - dateNow.getTime()) % (days) % (hours) / (mins)) + " minutes "
+	+ Math.floor((dateNewYear.getTime() - dateNow.getTime()) % (days) % (hours) % (mins) / (secs)) + " seconds");
+	
+console.log("Updated.");
