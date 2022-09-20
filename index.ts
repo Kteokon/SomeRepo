35cca25cf0787a5de02ecf3107e0cd01ddafa0ed
”Maksimova <ktoenok0801@gmail.com>
2022-09-20 22:18:20 +0800

    The function was exported and imported

--- index.ts
+++ index.ts
@@ -1,15 +1,3 @@
-function daysLeft(date){
-    const dateNewYear = new Date(date.getFullYear() + 1, 0, 1, 0, 0, 0, 0);
-        
-    const secs = 1000,
-        mins = secs * 60,
-        hours = mins * 60,
-        days = hours * 24;
-    
-    console.log("Time left before New Year: " + Math.floor((dateNewYear.getTime() - date.getTime()) / (days)) + " days "
-        + Math.floor((dateNewYear.getTime() - date.getTime()) % (days) / (hours)) + " hours "
-        + Math.floor((dateNewYear.getTime() - date.getTime()) % (days) % (hours) / (mins)) + " minutes "
-        + Math.floor((dateNewYear.getTime() - date.getTime()) % (days) % (hours) % (mins) / (secs)) + " seconds");
-}
+import {daysLeft} from './the_function';
 
 daysLeft(new Date());
